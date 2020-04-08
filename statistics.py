9447489df699cc0f033061a2c90c7985b1d9355d
Michael Delgado <devmikedel@gmail.com>
2020-04-07 20:06:06 -0500

    Add most_common_word and avg_num_words_per_sentence functions

--- statistics.py
+++ statistics.py
@@ -4,13 +4,16 @@ from typing import Dict
 import requests
 
 
+punctuation_list = ['.', ',', '!', '?', ';', ':', '"', '\'', '[', ']', '{', '}',
+               '\\', '|', '=', '+', '‒', '–', '—', '―', '(', ')', '*', '~', '&']
+
+
 class MessageBoardAPIWrapper:
     """
     Wrapper around the messageboard API
 
     http://localhost:8080/api/
     """
-
     response = requests.get('http://localhost:8080/api/messages')
 
     def __init__(self):
@@ -20,6 +23,7 @@ class MessageBoardAPIWrapper:
         """
         Returns the total number of messages.
         """
+        #Try to return the length of the response object
         try:
             return len(self.response)
         except Exception as e:
@@ -31,13 +35,47 @@ class MessageBoardAPIWrapper:
         """
         Returns the most frequently used word in messages.
         """
-        raise NotImplementedError
+        try:
+            word_list = []
+            for result in self.response:
+                for word in result['content'].split():
+                    # This checks if the first letter of the word is in the 
+                    # punctuation_list. If it is, the function replaces the letter 
+                    # with an empty string.
+                    if word[0] in punctuation_list:
+                        word = word.replace(word[0], '')
+                    # This checks if the last letter of the word is in the 
+                    # punctuation_list. If it is, the function replaces the letter 
+                    # with an empty string.
+                    elif word[-1] in punctuation_list:
+                        word = word.replace(word[-1], '')
+                    word_list.append(word.lower())
+            return max(set(word_list), key = word_list.count)
+
+        except Exception as e:
+            raise e
+        else:
+            raise NotImplementedError
 
     def avg_num_words_per_sentence(self) -> float:
         """
         Returns the average number of words per sentence.
         """
-        raise NotImplementedError
+        try:
+            content = []
+            words_per_sentence = []
+            for result in self.response:
+                content.append(result['content'])
+            content = "".join(content).split('.')
+            for sentence in content:
+                words_per_sentence.append(len(sentence.split()))
+            avg_num = sum(words_per_sentence) / len(words_per_sentence)
+            return int(round(avg_num, 0))
+
+        except Exception as e:
+            raise e
+        else:
+            raise NotImplementedError
 
     def avg_num_msg_thread_topic(self) -> Dict[str, float]:
         """
@@ -68,10 +106,10 @@ def main():
 
     print(f"Total number of messages: {messageboard.num_messages()}")
     print(f"Most common word: {messageboard.most_common_word()}")
-    # print(
-    #     f"Avg. number of words per sentence.:"
-    #     f"{messageboard.avg_num_words_per_sentence()}"
-    # )
+    print(
+        f"Avg. number of words per sentence.:"
+        f"{messageboard.avg_num_words_per_sentence()}"
+    )
     # print(
     #     f"Avg. number of messages per thread, per topic.:"
     #     f"{messageboard.avg_num_msg_thread_topic()}"
